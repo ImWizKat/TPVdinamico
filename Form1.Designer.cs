6c775426240f64f8c12eda5f3865a9728eee3b25
Daniel Calvo <ImWizKat@gmail.com>
2021-09-29 12:53:17 +0200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace TPVdinámico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Producto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PrecioUnitario = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Peso = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ValorParcial = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonNuevoCliente = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.buttonEmail = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Producto,
+            this.PrecioUnitario,
+            this.Peso,
+            this.ValorParcial});
+            this.dataGridView1.Location = new System.Drawing.Point(500, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 150);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Producto
+            // 
+            this.Producto.HeaderText = "Producto";
+            this.Producto.Name = "Producto";
+            // 
+            // PrecioUnitario
+            // 
+            this.PrecioUnitario.HeaderText = "PrecioUnitario";
+            this.PrecioUnitario.Name = "PrecioUnitario";
+            // 
+            // Peso
+            // 
+            this.Peso.HeaderText = "Peso";
+            this.Peso.Name = "Peso";
+            // 
+            // ValorParcial
+            // 
+            this.ValorParcial.HeaderText = "ValorParcial";
+            this.ValorParcial.Name = "ValorParcial";
+            // 
+            // buttonNuevoCliente
+            // 
+            this.buttonNuevoCliente.Location = new System.Drawing.Point(603, 295);
+            this.buttonNuevoCliente.Name = "buttonNuevoCliente";
+            this.buttonNuevoCliente.Size = new System.Drawing.Size(83, 79);
+            this.buttonNuevoCliente.TabIndex = 4;
+            this.buttonNuevoCliente.Text = "Nuevo Cliente";
+            this.buttonNuevoCliente.UseVisualStyleBackColor = true;
+            this.buttonNuevoCliente.Click += new System.EventHandler(this.buttonNuevoCliente_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(848, 305);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "0";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(470, 362);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // buttonEmail
+            // 
+            this.buttonEmail.Location = new System.Drawing.Point(715, 295);
+            this.buttonEmail.Name = "buttonEmail";
+            this.buttonEmail.Size = new System.Drawing.Size(83, 79);
+            this.buttonEmail.TabIndex = 7;
+            this.buttonEmail.Text = "Enviar Factura";
+            this.buttonEmail.UseVisualStyleBackColor = true;
+            this.buttonEmail.Click += new System.EventHandler(this.buttonEmail_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(947, 375);
+            this.Controls.Add(this.buttonEmail);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonNuevoCliente);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Producto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PrecioUnitario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Peso;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ValorParcial;
+        private System.Windows.Forms.Button buttonNuevoCliente;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonEmail;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,158 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TPVdinámico
+{
+    public partial class Form1 : Form
+    {
+
+        String[] nombres = { "chirimoya", "ciruela", "fresa", "kiwi", "mandarina", "melocoton", "melon", "naranja", "nectarina", "papaya", "peras", "piña", "peras", "piña", "platanos", "pomelos", "prunus", "sandias" };
+
+        String[] precios = { "1,25", "2,75", "3,25", "1,50", "2,75", "3,25", "1", "2", "3", "1", "2", "3", "1", "2", "3", "1", "2", "3" };
+
+        decimal total = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            //generar los botones y campos de texto
+
+            for (int a = 0; a < 15; a++)
+            {
+                Button btnX = new Button();                
+                btnX.Name = Convert.ToString(a);
+                btnX.Text = nombres[a];
+                btnX.Image = System.Drawing.Image.FromFile(@"imagenes\" + btnX.Text + ".PNG");
+                btnX.BackgroundImageLayout = ImageLayout.Zoom;
+                btnX.Tag = precios[a];
+                btnX.Size = new Size(65,65);
+                btnX.Location = new Point(a * 75, 25);
+
+                TextBox txtX = new TextBox();
+                txtX.Enabled = false;
+                txtX.Name = Convert.ToString(a);
+                txtX.Text = precios[a];
+                txtX.Size = new Size(70, 70);
+                txtX.Location = new Point(a * 75, 25);
+
+                flowLayoutPanel1.Controls.Add(btnX);
+                flowLayoutPanel1.Controls.Add(txtX);
+                btnX.Click += btnXClick;
+            }
+        }
+
+        private void btnXClick(object sender, EventArgs e)
+        {
+            Button btnX = (Button)sender;
+
+            String peso = Interaction.InputBox("Introduce kgs de " + btnX.Text, "TPV", "0");
+
+            String precio = btnX.Tag.ToString();
+
+            String[] fila = { btnX.Text,
+                                precio,
+                                peso,
+                                (Convert.ToDecimal(peso) * Convert.ToDecimal(precio)).ToString()};
+
+            dataGridView1.Rows.Add(fila);
+
+            total += Convert.ToDecimal(fila[3]);
+
+            label1.Text = total.ToString();
+
+        }
+
+        private void buttonNuevoCliente_Click(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void reset()
+        {
+            total = 0;
+            label1.Text = total.ToString();
+            dataGridView1.Rows.Clear();
+        }
+
+        private void buttonEmail_Click(object sender, EventArgs e)
+        {
+            generarPDF();
+        }
+
+        private void generarPDF()
+        {
+            //DataGridView----->PDF(utiliza la librería iTextSharp)
+
+        //Crear Tabla iTextSharp  desde una tabla de datos (datagridView)
+            PdfPTable pdfTable = new PdfPTable(dataGridView1.ColumnCount);
+
+            //padding
+            pdfTable.DefaultCell.Padding = 3;
+
+            //ancho que va a ocupar la tabla en el pdf
+            pdfTable.WidthPercentage = 30;
+
+            //alineación
+            pdfTable.HorizontalAlignment = Element.ALIGN_LEFT;
+
+            //borde de las tablas
+            pdfTable.DefaultCell.BorderWidth = 1;
+            //Añadir fila de cabecera
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                PdfPCell cell = new PdfPCell(new Phrase(column.HeaderText));
+                cell.BackgroundColor = new iTextSharp.text.BaseColor(240, 240, 240);
+                pdfTable.AddCell(cell);
+            }
+
+            //añadir filas
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    try
+                    {
+                        pdfTable.AddCell(cell.Value.ToString());
+                    }
+                    catch { pdfTable.AddCell("total"); }
+                }
+            }
+
+            //Exportar a pdf (ruta por defect
+            string folderPath = "C:\\Users\\David\\Desktop\\Tickets";
+
+            //si no existe el directoria se crea
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+            using (FileStream stream = new FileStream(folderPath + "\\ticket.pdf", FileMode.Create))
+            {
+                Document pdfDoc = new Document(PageSize.A2, 10f, 10f, 10f, 0f);
+                PdfWriter.GetInstance(pdfDoc, stream);
+                pdfDoc.Open();
+                pdfDoc.Add(pdfTable);
+                pdfDoc.Close();
+                stream.Close();
+            }
+        }
+    }
+}
+    
